derivatives all fixed

common.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 TRAIN_TO_VALIDATE_RATIO = 4
-TEST_SIZE = 3 # 80 # 200
+TEST_SIZE = 200
 
 # verbosity of convex optimization solver
 VERBOSE = False
@@ -85,6 +85,8 @@ def get_nonzero_indices(some_vector, threshold=CLOSE_TO_ZERO_THRESHOLD):
     return np.reshape(np.array(np.greater(np.abs(some_vector), threshold).T), (some_vector.size, ))
 
 def get_norm2(vector, power=1):
+    # print "vector", vector
+    # print "vector", vector.shape
     return np.power(np.linalg.norm(vector, ord=None), power)
 
 #### RANDOM TESTS

convexopt_solvers.py
@@ -426,12 +426,14 @@ class GenAddModelProblemWrapper:
     def __init__(self, X, train_indices, y, tiny_e=1e-10):
         self.tiny_e = tiny_e
 
+        self.y = y
+
         num_samples, num_features = X.shape
         self.num_samples = num_samples
         self.num_features = num_features
 
         # Create smooth penalty matrix for each feature
-        self.diff_matrices = [0] * num_features
+        self.diff_matrices = []
         for i in range(num_features):
             x_features = X[:,i]
             d1_matrix = np.zeros((num_samples, num_samples))
@@ -440,59 +442,64 @@ class GenAddModelProblemWrapper:
             ordered_x = x_features[sample_ordering]
             d1_matrix[range(num_samples - 1), sample_ordering[:-1]] = -1
             d1_matrix[range(num_samples - 1), sample_ordering[1:]] = 1
-            print "sample_ordering", sample_ordering
-            print "d1_matrix", d1_matrix
             inv_dists = 1.0 / (ordered_x[np.arange(1, num_samples)] - ordered_x[np.arange(num_samples - 1)])
             inv_dists = np.append(inv_dists, 0)
-            print "inv_dists", inv_dists
 
             # Check that the inverted distances are all greater than zero
             assert(np.min(inv_dists) >= 0)
+            D = d1_matrix * np.matrix(np.diagflat(inv_dists)) * d1_matrix
+            self.diff_matrices.append(D)
 
-            self.diff_matrices[i] = (
-                d1_matrix * np.matrix(np.diagflat(inv_dists)) * d1_matrix
-            )
-
+        self.train_indices = train_indices
         self.train_identifier = np.matrix(np.zeros((len(train_indices), num_samples)))
-        num_train = len(train_indices)
-        self.train_identifier[np.arange(num_train), train_indices] = 1
-
-        self.y = y
-        self.thetas = Variable(num_samples, num_features)
-        self.lambdas = [Parameter(sign="positive")] * num_features
-        print "num_train", num_train
-        print "num_samples", num_samples
-        objective = 0.5/num_train * sum_squares(y - self.train_identifier * sum_entries(self.thetas, axis=1))
-        for i in range(num_features):
-            D = sp.sparse.coo_matrix(self.diff_matrices[i])
-            D_sparse = cvxopt.spmatrix(D.data, D.row.tolist(), D.col.tolist())
-            objective += 0.5/num_samples * self.lambdas[i] * sum_squares(D_sparse * self.thetas[:,i])
-        objective += 0.5 * self.tiny_e/(num_features * num_samples) * sum_squares(self.thetas)
-
-        self.problem = Problem(Minimize(objective), [])
+        self.num_train = len(train_indices)
+        self.train_identifier[np.arange(self.num_train), train_indices] = 1
 
     def solve(self, lambdas):
-        print "cvxpy solving... lambdas", lambdas
-        for i in range(0, len(lambdas)):
-            self.lambdas[i].value = lambdas[i]
+        thetas = Variable(self.num_samples, self.num_features)
+        lambdas = [Parameter(sign="positive", value=l) for l in lambdas]
+        objective = 0.5/self.num_train * sum_squares(self.y - sum_entries(thetas[self.train_indices,:], axis=1))
+        for i in range(len(lambdas)):
+            D = sp.sparse.coo_matrix(self.diff_matrices[i])
+            D_sparse = cvxopt.spmatrix(D.data, D.row.tolist(), D.col.tolist())
+            objective += 0.5/self.num_samples * lambdas[i] * sum_squares(D_sparse * thetas[:,i])
+        objective += 0.5 * self.tiny_e/(self.num_features * self.num_samples) * sum_squares(thetas)
+        self.problem = Problem(Minimize(objective))
+        self.problem.solve()
+        # print "basic problem.value", self.problem.value
+        # print "self.thetas", thetas
+        # print get_norm2(self.diff_matrices[0] * thetas.value)
+        # print ".5/self.num_samples * lam * get_norm2(D * theta, power=2)", .5/self.num_samples * lambdas[0].value * get_norm2(self.diff_matrices[0] * thetas.value, power=2)
 
-        result = self.problem.solve(solver=SCS, verbose=VERBOSE, max_iters=SCS_MAX_ITERS) #, use_indirect=False, normalize=True)
-        print "self.problem.status", self.problem.status
-        return self.thetas.value
+        print "cvxpy, self.problem.status", self.problem.status, "value", self.problem.value
+        self.lambdas = lambdas
+        self.thetas = thetas.value
+        return thetas.value
 
     def get_cost_components(self):
         total_train_cost = self.problem.value
         num_train = self.y.size
+        print "num_train", num_train
+        print "self.num_samples", self.num_samples
+        print "self.thetas.value", self.thetas
         train_loss = 0.5/num_train * get_norm2(
-            self.y - self.train_identifier * np.sum(self.thetas.value, axis=1)
+            self.y - self.train_identifier * np.sum(self.thetas, axis=1),
+            power=2
         )
         penalties = []
         for i in range(self.num_features):
-            theta = self.thetas.value[:,i]
+            theta = np.matrix(self.thetas[:,i])
             lam = self.lambdas[i].value
             D = self.diff_matrices[i]
             penalties.append(
                 .5/self.num_samples * lam * get_norm2(D * theta, power=2)
             )
-        tiny_e_cost = 0.5 * self.tiny_e/(self.num_features * self.num_samples) * get_norm2(self.thetas.value, power=2)
+        tiny_e_cost = 0.5 * self.tiny_e/(self.num_features * self.num_samples) * get_norm2(self.thetas, power=2)
+        print "cost_components:"
+        print "tot", total_train_cost
+        print "t", train_loss
+        print "p", penalties
+        print "e", tiny_e_cost
+        print "diff", np.abs(total_train_cost - train_loss - sum(penalties) - tiny_e_cost)
+        assert(np.abs(total_train_cost - train_loss - sum(penalties) - tiny_e_cost) < 0.05)
         return total_train_cost, train_loss, penalties, tiny_e_cost

gen_add_model_hillclimb.py
@@ -7,15 +7,15 @@ from common import *
 from convexopt_solvers import GenAddModelProblemWrapper
 
 class GenAddModelHillclimb:
-    NUMBER_OF_ITERATIONS = 60
+    NUMBER_OF_ITERATIONS = 30 # 60
     BOUNDARY_FACTOR = 0.8
-    STEP_SIZE = 0.5
+    STEP_SIZE = 1 # 0.5
     LAMBDA_MIN = 1e-6
     SHRINK_MIN = 1e-3 # 1e-15
     METHOD_STEP_SIZE_MIN = 1e-32
     SHRINK_SHRINK_FACTOR = 0.1
     SHRINK_FACTOR_INIT = 1
-    DECREASING_ENOUGH_THRESHOLD = 1e-3
+    DECREASING_ENOUGH_THRESHOLD = 1e-4
     METHOD_LABEL = "HC_Generalized_additive_model"
 
     def __init__(self, X_train, y_train, X_validate, y_validate, X_test):
@@ -59,14 +59,16 @@ class GenAddModelHillclimb:
         potential_thetas = None
         for i in range(0, self.NUMBER_OF_ITERATIONS):
             print "ITER", i
-            print "get_cost_components", self.problem_wrapper.get_cost_components()
-            lambda_derivatives = self._get_lambda_derivatives(curr_regularization)
+            # print "get_cost_components", self.problem_wrapper.get_cost_components()
+            lambda_derivatives = self._get_lambda_derivatives(curr_regularization, thetas)
             if debug:
                 check_derivs = self._double_check_derivative(curr_regularization)
+                print "lambda_derivatives", lambda_derivatives
+                print "numeric derivs", check_derivs
                 for j, check_d in enumerate(check_derivs):
                     print "lambda_derivatives", lambda_derivatives[j]
-                    print "check_derivs", check_d
-                    assert(np.abs(check_d - lambda_derivatives[j]) < 0.01)
+                    print "numeric_deriv", check_d
+                    assert(np.abs(check_d - lambda_derivatives[j]) < 0.005)
             assert(not np.any(np.isnan(lambda_derivatives)))
 
             potential_new_regularization = self._get_updated_lambdas(
@@ -131,11 +133,13 @@ class GenAddModelHillclimb:
 
         return thetas, cost_path, curr_regularization
 
-    def _get_lambda_derivatives(self, curr_lambdas):
-        print "_get_lambda_derivatives"
+    def _get_lambda_derivatives(self, curr_lambdas, curr_thetas):
+        print "_get_lambda_derivatives, curr_lambdas", curr_lambdas
         H = np.tile(self.MM, (self.num_features, self.num_features))
-        print "H tile MM, sum", np.sum(H)
-        H += self.problem_wrapper.tiny_e/(self.num_features * self.num_samples) * np.eye(self.num_features * self.num_samples)
+        num_feat_sam = self.num_features * self.num_samples
+        H += self.problem_wrapper.tiny_e/num_feat_sam * np.eye(num_feat_sam)
+        # print "curr_lambdas[i] * self.DD[i]", curr_lambdas[0] * self.DD[0]
+        # print "sp.linalg.block_diag", sp.linalg.block_diag(*[curr_lambdas[i] * self.DD[i] for i in range(self.num_features)])
         H += sp.linalg.block_diag(*[
             curr_lambdas[i] * self.DD[i] for i in range(self.num_features)
         ])
@@ -146,36 +150,33 @@ class GenAddModelHillclimb:
         # print sp.linalg.block_diag(*[
         #     curr_lambdas[i] * self.DD[i] for i in range(self.num_features)
         # ])
-        H = sp.sparse.csr_matrix(H)
+        # H = sp.sparse.csr_matrix(H)
 
-        sum_thetas = np.matrix(np.sum(self.problem_wrapper.thetas.value, axis=1))
-        # print "self.problem_wrapper.thetas", self.problem_wrapper.thetas.value
-        # print "sum_thetas", sum_thetas
+        sum_thetas = np.matrix(np.sum(curr_thetas, axis=1))
         dloss_dlambdas = []
         num_validate = self.y_validate.size
         for i in range(self.num_features):
-            print "=========I======", i
-            theta = self.problem_wrapper.thetas.value[:,i]
-            b = np.zeros((self.num_samples * self.num_features, 1))
-            b[i * self.num_samples:(i + 1) * self.num_samples, :] = -self.DD[i] * theta
+            # print "=========I======", i
+            b = np.zeros((num_feat_sam, 1))
+            b[i * self.num_samples:(i + 1) * self.num_samples, :] = -self.DD[i] * curr_thetas[:,i]
             # print "b", b
             # dtheta_dlambdai = sp.linalg.solve(H, b, sym_pos=True)
-            # dtheta_dlambdai = np.linalg.solve(H, b)
+            dtheta_dlambdai = np.linalg.solve(H, b)
             # dtheta_dlambdai, _, _, _ = np.linalg.lstsq(H, b)
-            dtheta_dlambdai = spsolve(H, b)
+            # dtheta_dlambdai = spsolve(H, b)
             # print "dtheta_dlambdai", dtheta_dlambdai
             dtheta_dlambdai = dtheta_dlambdai.reshape((self.num_features, self.num_samples)).T
             # print "dtheta_dlambdai reshaped", dtheta_dlambdai
             sum_dtheta_dlambdai = np.matrix(np.sum(dtheta_dlambdai, axis=1)).T
-            # print "sum_dtheta_dlambdai", sum_dtheta_dlambdai.shape
-            # print "self.y_validate", self.y_validate
             # print "sum_dtheta_dlambdai", sum_dtheta_dlambdai
+            # print "self.y_validate", self.y_validate
             # print "sum_thetas for validate", sum_thetas[self.validate_idx,:]
             # print "self.y_validate", self.y_validate
             # dloss_dlambdai = -1.0/num_validate * (self.validate_M * sum_dtheta_dlambdai).T * (self.y_validate - self.validate_M * sum_thetas)
             dloss_dlambdai = -1.0/num_validate * sum_dtheta_dlambdai[self.validate_idx].T * (self.y_validate - sum_thetas[self.validate_idx])
-            print "dloss_dlambdai", dloss_dlambdai
+            # print "dloss_dlambdai", dloss_dlambdai
             dloss_dlambdas.append(dloss_dlambdai[0,0])
+            # break
 
         print "dloss_dlambdas", dloss_dlambdas
         return np.array(dloss_dlambdas)
@@ -193,10 +194,11 @@ class GenAddModelHillclimb:
 
         return np.maximum(lambdas - new_step_size * lambda_derivatives, self.LAMBDA_MIN)
 
-    def _double_check_derivative(self, regularization, epsilon=0.00001):
+    def _double_check_derivative(self, regularization, epsilon=0.0001):
         print "double_check_derivative"
         deriv = []
         for i in range(len(regularization)):
+            print "===========CHECK I= %d ===============" % i
             reg1 = [r for r in regularization]
             reg1[i] += epsilon
             thetas1 = self.problem_wrapper.solve(reg1)
@@ -206,13 +208,10 @@ class GenAddModelHillclimb:
             reg2[i] -= epsilon
             thetas2 = self.problem_wrapper.solve(reg2)
             error2 = testerror_multi_smooth(self.y_validate, self.validate_idx, thetas2)
-            print "error1", error1, "error2", error2
             print "(error1 - error2)/(epsilon * 2)", (error1 - error2)/(epsilon * 2)
             deriv.append(
                 (error1 - error2)/(epsilon * 2)
             )
-            print "thetas1", thetas1
-            print "thetas2", thetas2
             print "(thetas1 - thetas2)/(epsilon * 2)", (thetas1 - thetas2)/(epsilon * 2)
         return deriv
 

threegam_eval.py
@@ -12,7 +12,7 @@ from common import *
 np.set_printoptions(edgeitems=3,infstr='inf', linewidth=200, nanstr='nan', precision=8, suppress=False, threshold=1000, formatter=None)
 
 np.random.seed(1)
-TRAIN_SIZE = 10
+TRAIN_SIZE = 300
 
 def identity_fcn(x):
     return x.reshape(x.size, 1)
@@ -21,6 +21,7 @@ def crazy_down_sin(x):
     return np.sin(np.power(x/2, 2)) - x
 
 def plot_res(fitted_thetas, fcn_list, X, outfile="figures/threegam/out.png"):
+    colors = ["green", "blue", "red"]
     num_features = fitted_thetas.shape[1]
     plt.clf()
     for i in range(num_features):
@@ -34,31 +35,33 @@ def plot_res(fitted_thetas, fcn_list, X, outfile="figures/threegam/out.png"):
             x_features[order_x],
             fitted_thetas[order_x,i], # fitted values
             '.',
-            label="feat %d" % i
+            label="feat %d" % i,
+            color=colors[i]
         )
     plt.savefig(outfile)
 
 
 #### Note: there seems to be an identifiability issue
 
-smooth_fcn_list = [crazy_down_sin, identity_fcn]
-# smooth_fcn_list = [identity_fcn]
+# smooth_fcn_list = [crazy_down_sin, np.sin, identity_fcn]
+smooth_fcn_list = [crazy_down_sin, np.sin]
+# smooth_fcn_list = [crazy_down_sin]
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = multi_smooth_features(
     TRAIN_SIZE,
     smooth_fcn_list,
-    desired_snr=1,
+    desired_snr=2,
     feat_range=[0,10]
 )
 X_full, train_idx, validate_idx, test_idx = GenAddModelHillclimb.stack((X_train, X_validate, X_test))
 
-print "start"
 hc = GenAddModelHillclimb(X_train, y_train, X_validate, y_validate, X_test)
-print "inited!"
-init_lambdas = np.array([0.25,0.25])
-hc_thetas, cost_path, curr_regularization = hc.run(init_lambdas, debug=True)
+init_lambdas = np.array([3, 3])
+hc_thetas, cost_path, curr_regularization = hc.run(init_lambdas, debug=False)
 hc_test_error = testerror_multi_smooth(y_test, test_idx, hc_thetas)
 plot_res(hc_thetas[test_idx], smooth_fcn_list, X_test, outfile="figures/threegam/out_hc.png")
+
+print "=================================================="
 
 gs_thetas, best_lambdas = gs.run(
     y_train,
@@ -67,7 +70,7 @@ gs_thetas, best_lambdas = gs.run(
     train_idx,
     validate_idx,
     num_lambdas=3,
-    max_lambda=10
+    max_lambda=30
 )
 
 gs_test_error = testerror_multi_smooth(y_test, test_idx, gs_thetas)