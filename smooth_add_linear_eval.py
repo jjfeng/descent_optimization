303264f9e86667a182278decddca285c111b4b70
Jean Feng <jean.feng@gmail.com>
2016-06-09 14:50:58 -0700

    remove seed

--- smooth_add_linear_eval.py
+++ smooth_add_linear_eval.py
@@ -86,7 +86,6 @@ def main(argv):
             assert(data_type in [0, 1, 2])
 
     seed = int(np.random.rand() * 1e5)
-    seed = 31288
     print "numpy rand seed", seed
     np.random.seed(seed)
 
