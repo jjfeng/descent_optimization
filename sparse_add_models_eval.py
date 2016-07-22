update derivative fixes. though not fixed enough yet

sparse_add_models_eval.py
@@ -33,12 +33,12 @@ def const_zero(x):
 
 
 def main(argv):
-    num_funcs = 3
-    num_zero_funcs = 3
-    train_size = 30
-    validate_size = 30
-    test_size = 30
-    snr = 2
+    num_funcs = 1
+    num_zero_funcs = 1
+    train_size = 3
+    validate_size = 3
+    test_size = 0
+    snr = 3
 
     try:
         opts, args = getopt.getopt(argv,"f:z:a:b:c:s:")
@@ -68,8 +68,8 @@ def main(argv):
     hc_results = MethodResults("Hillclimb")
 
     hc_algo = Sparse_Add_Model_Hillclimb(observed_data)
-    initial_lambdas = np.ones(1 + num_funcs + num_zero_funcs)
-    hc_algo.run(initial_lambdas)
+    initial_lambdas = np.ones(1 + num_funcs + num_zero_funcs) * 0.01
+    hc_algo.run(initial_lambdas, debug=True)
 
     print "===========RUN ============"
     hc_results.print_results()

sparse_add_models_hillclimb.py
@@ -30,10 +30,6 @@ class Sparse_Add_Model_Hillclimb(Gradient_Descent_Algo):
             self.data.train_idx,
             self.data.y_train
         )
-        self.I_tiled = np.matrix(
-            np.tile(np.eye(self.data.num_samples), (self.data.num_features, 1))
-        )
-        self.II = self.I_tiled * self.I_tiled.T
 
     def get_validate_cost(self, model_params):
         return testerror_sparse_add_smooth(
@@ -43,53 +39,118 @@ class Sparse_Add_Model_Hillclimb(Gradient_Descent_Algo):
         )
 
     def _get_lambda_derivatives(self):
-        nonzero_thetas_idx = self._get_nonzero_theta_indices(self.fmodel.current_model_params)
+        nonzero_thetas_idx = self._get_nonzero_theta_vectors(self.fmodel.current_model_params)
+        nonzero_thetas_idx = np.array(nonzero_thetas_idx)
+        print "nonzero_thetas_idx", nonzero_thetas_idx
 
-        dtheta_dlambda = self._get_dtheta_dlambda(self.fmodel.current_model_params[:, nonzero_thetas_idx])
-        fitted_y_validate = np.sum(self.fmodel.current_model_params[self.data.validate_idx], axis=1)
-        print dtheta_dlambda.shape
-        dloss_dlambda = -1.0/self.data.num_validate * (
-            dtheta_dlambda[self.data.validate_idx, :].T * (self.data.y_validate - fitted_y_validate)
+        dtheta_dlambda = self._get_dtheta_dlambda(nonzero_thetas_idx)
+        fitted_y_validate = np.sum(self.fmodel.current_model_params[self.data.validate_idx, :], axis=1)
+        dloss_dlambda = -1 * (
+            np.sum(dtheta_dlambda[self.data.validate_idx, :, :], axis=1).T * (self.data.y_validate - fitted_y_validate)
         )
+        print "dloss_dlambda", dloss_dlambda
         return dloss_dlambda.A1 # flatten the matrix
 
-    def _get_dtheta_dlambda(self, nonzero_thetas):
+    def _get_dtheta_dlambda(self, nonzero_thetas_idx):
         def create_b_diag_elem(i):
-            theta = nonzero_thetas[i,:]
-            theta_norm = np.linalg.norm(theta, 2)
-            b_diag_elem = 1/theta_norm * (np.eye(self.data.num_samples) - theta * theta.T/(theta_norm**2))
-            return b_diag_elem
-
-        def normalize_theta(i):
+            # theta = self.fmodel.current_model_params[:,i]
             theta = nonzero_thetas[:,i]
             theta_norm = np.linalg.norm(theta, 2)
-            return theta/theta_norm
+            if theta_norm < 1e-10:
+                return np.matrix(np.zeros((self.data.num_samples, self.data.num_samples)))
+            else:
+                print "np.eye(self.data.num_samples)", np.eye(self.data.num_samples)
+                print "theta", theta
+                print "theta * theta", theta * theta.T
+                print "theta_norm**2", theta_norm, theta_norm**2
+                b_diag_elem = 1/theta_norm * (np.eye(self.data.num_samples) - theta * theta.T/(theta_norm**2))
+                return b_diag_elem
+
+        def normalize_theta(i):
+            # theta = self.fmodel.current_model_params[:,i]
+            theta = nonzero_thetas[:,i]
+            print "theta", theta
+            theta_norm = np.linalg.norm(theta, 2)
+            if theta_norm < 1e-10:
+                return np.zeros(theta.shape)
+            else:
+                return theta/theta_norm
 
         def make_diag_dtheta(i):
-            dd_theta = self.problem_wrapper.diff_matrices[i].T * np.sign(
-                self.problem_wrapper.diff_matrices[i] * nonzero_thetas[:,i]
-            )
+            # theta = self.fmodel.current_model_params[:,i]
+            theta = nonzero_thetas[:,i]
+            zeroed_thetas = self._zero_theta_indices(self.problem_wrapper.diff_matrices[i] * theta)
+            dd_theta = self.problem_wrapper.diff_matrices[i].T * np.sign(zeroed_thetas)
             return dd_theta
 
+        nonzero_thetas = self.fmodel.current_model_params[:, nonzero_thetas_idx]
         num_nonzero_thetas = nonzero_thetas.shape[1]
         b_diag_elems = map(create_b_diag_elem, range(num_nonzero_thetas))
+        print "b_diag_elems", b_diag_elems
         b_diag = sp.linalg.block_diag(*b_diag_elems)
 
+        # normalized_thetas = map(normalize_theta, range(self.data.num_features))
         normalized_thetas = map(normalize_theta, range(num_nonzero_thetas))
         normalized_thetas = np.vstack(normalized_thetas)
+        print "normalized_thetas", normalized_thetas
 
         diag_dthetas = map(make_diag_dtheta, range(num_nonzero_thetas))
         diag_dthetas = sp.linalg.block_diag(*diag_dthetas)
+        print "np.logical_not(nonzero_thetas_idx)", np.where(np.logical_not(nonzero_thetas_idx))
+        print "diag_dthetas BEFORE", diag_dthetas
+        print "INSERT", np.insert(diag_dthetas, np.where(np.logical_not(nonzero_thetas_idx))[0], np.zeros((diag_dthetas.shape[0], 1)), axis=1)
+        diag_dthetas = np.insert(diag_dthetas, np.where(np.logical_not(nonzero_thetas_idx))[0], np.zeros((diag_dthetas.shape[0], 1)), axis=1)
+        print "diag_dthetas", diag_dthetas
 
         rhs_matrix = np.hstack((normalized_thetas, diag_dthetas))
+        print "rhs_matrix", rhs_matrix
 
         lambda0 = self.fmodel.current_lambdas[0]
-        dtheta_dlambda, _, _, _ = np.linalg.lstsq(self.II + lambda0 * b_diag, -1 * rhs_matrix)
+        II = np.matrix(
+            np.tile(np.eye(self.data.num_samples), (num_nonzero_thetas, num_nonzero_thetas))
+        )
+        assert(II.shape == b_diag.shape)
+        dtheta_dlambda, _, _, _ = np.linalg.lstsq(II + lambda0 * b_diag, -1 * rhs_matrix)
+        print "dtheta_dlambda", dtheta_dlambda
+        # dtheta_dlambda = np.asarray(dtheta_dlambda).reshape(self.data.num_samples, self.data.num_features, self.fmodel.num_lambdas, order="F")
+        dtheta_dlambda = np.asarray(dtheta_dlambda).reshape(self.data.num_samples, num_nonzero_thetas, self.fmodel.num_lambdas, order="F")
+        print "dtheta_dlambda", dtheta_dlambda[:,:,0]
+        print "dtheta_dlambda", dtheta_dlambda[:,:,1]
         return dtheta_dlambda
 
-    def _double_check_derivative(self, calculated_derivative):
-        return True
+    def _double_check_derivative(self, calculated_derivative, accept_diff=1e-3, epsilon=1e-6):
+        print "double_check_derivative"
+        deriv = []
+        num_lambdas = len(self.fmodel.current_lambdas)
+        for i in range(num_lambdas):
+            print "===========CHECK I= %d ===============" % i
+            reg1 = [r for r in self.fmodel.current_lambdas]
+            reg1[i] += epsilon
+            thetas1 = self.problem_wrapper.solve(reg1)
+            error1 = self.get_validate_cost(thetas1)
+
+            reg2 = [r for r in self.fmodel.current_lambdas]
+            reg2[i] -= epsilon
+            thetas2 = self.problem_wrapper.solve(reg2)
+            error2 = self.get_validate_cost(thetas2)
+            i_deriv = (error1 - error2)/(epsilon * 2)
+            deriv.append(i_deriv)
+            print "thetas1 norm", np.linalg.norm(thetas1[:,0]), thetas1[:,0]
+            print "thetas2 norm", np.linalg.norm(thetas2[:,0]), thetas2[:,0]
+            print "(thetas1 - thetas2)/(epsilon * 2)", (thetas1 - thetas2)/(epsilon * 2)
+            print "calculated_derivative[i]", calculated_derivative[i]
+            print "numerical deriv", i_deriv
+            assert(np.abs(calculated_derivative[i] - i_deriv) < accept_diff)
+
+        return deriv
 
     @staticmethod
-    def _get_nonzero_theta_indices(thetas, threshold=CLOSE_TO_ZERO_THRESHOLD):
-        return map(lambda i: np.linalg.norm(thetas[:,i]) > threshold, range(thetas.shape[1]))
+    def _get_nonzero_theta_vectors(thetas, threshold=1e-10):
+        for i in range(thetas.shape[1]):
+            print "thetas", np.linalg.norm(thetas[:,i])
+        nonzero_thetas_idx = map(lambda i: np.linalg.norm(thetas[:,i]) > threshold, range(thetas.shape[1]))
+        return np.array(nonzero_thetas_idx)
+
+    @staticmethod
+    def _zero_theta_indices(theta, threshold=1e-10):
+        return np.multiply(theta, np.greater(np.abs(theta), threshold))