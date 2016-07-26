ff80cdc343f86f52cfaed414c45d97e2cd736a7e
Jean Feng <jean.feng@gmail.com>
2016-07-25 20:31:31 -0700

    add tmp folder

--- sparse_add_models_eval.py
+++ sparse_add_models_eval.py
@@ -141,7 +141,7 @@ def fit_data_for_iter(iter_data):
         method,
         iter_data.i
     )
-    log_file_name = "%s/log_%s.txt" % (settings.results_folder, str_identifer)
+    log_file_name = "%s/tmp/log_%s.txt" % (settings.results_folder, str_identifer)
     print "log_file_name", log_file_name
     with open(log_file_name, "w") as f:
         if method == "NM":
